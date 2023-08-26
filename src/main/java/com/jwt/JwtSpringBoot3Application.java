94ea49c440175a064c97a9048b4c919ed6b74cfa
krtuhin <tuhin.km2774@gmail.com>
2023-08-27 02:14:00 +0530

    Project Created

---
+++ src/main/java/com/jwt/JwtSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSpringBoot3Application.class, args);
+    }
+
+}
